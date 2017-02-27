227697de07abebbfbec422e916d07bc00aacb51b
Catherine J <cjarocki@gmail.com>
2017-02-26 19:20:34 -0600

    Finished buttons, logic works, worked on styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,88 +1,118 @@
-console.log('ready to jquery');
+// console.log('ready to jquery');
 
 var values = {};
-// var urlMath =
 
 $(document).ready(function(){
 
-  $('.operation').on('click', function() {
+  $('#addition').on('click', function(){
     values.a = $('#firstNumber').val();
     values.b = $('#secondNumber').val();
     values.type = 'addition';
+    if(values.type == 'addition'){
       additionTest();
-    values.type = 'subtraction';
-      subtractionTest();
-      values.type = 'multiplication';
-      multiplicationTest();
-    values.type = 'division';
-      divisionTest();
-
-    console.log("el clic funciona!");
-
-    function additionTest(){
-      $.ajax({
-        url: '/addition',
-        type: 'POST',
-        data: values,
-        success: function(response){
-           answerOnDom(response);
-        }
-     }); //ends ajax
+      // console.log(additionTest);
     }
-
-    function subtractionTest(){
-      $.ajax({
-        url: '/subtraction',
-        type: 'POST',
-        data: values,
-        success: function(response){
-           answerOnDom(response);
-        }
-     }); //ends ajax
-    }
-
-    function multiplicationTest(){
-      $.ajax({
-        url: '/multiplication',
-        type: 'POST',
-        data: values,
-        success: function(response){
-           result(response);
-        }
-     }); //ends ajax
-    }
-
-    function divisionTest(){
-      $.ajax({
-        url: '/division',
-        type: 'POST',
-        data: values,
-        success: function(response){
-           result();
-        }
-     }); //ends ajax
-    }
-
   });
 
-  // var result = function(){
-  //     $('#answer').append(response);
-  // };
+  $('#subtraction').on('click', function(){
+    values.a = $('#firstNumber').val();
+    values.b = $('#secondNumber').val();
+    values.type = 'subtraction';
+    if(values.type == 'subtraction'){
+      subtractionTest();
+      // console.log(subtractionTest);
+    }
+  });
 
-  // $('#clearAnswer').on('click', function(){
-  //   $('span').text("");
-  // });
-  // $('#numberContainer').on('click', function(){
-      // var newObject = {};
-      // newObject.name = $('#newName').val();
-      // $.ajax({
-      //   type: 'POST',
-      //   url: '/fish/new',
-      //   data: newFishObject,
-      //   success: function(response){
-          // console.log(response);
-      //     getFishData();
-      //   }
-    // }); //ends success response fxn
+  $('#multiplication').on('click', function(){
+    values.a = $('#firstNumber').val();
+    values.b = $('#secondNumber').val();
+    values.type = 'multiplication';
+    if(values.type == 'multiplication'){
+      multiplicationTest();
+      // console.log(multiplicationTest);
+    }
+  });
 
-}); //ends doc ready
+  $('#division').on('click', function(){
+    values.a = $('#firstNumber').val();
+    values.b = $('#secondNumber').val();
+    values.type = 'division';
+    if(values.type == 'division'){
+      divisionTest();
+      // console.log(divisionTest);
+    }
+  });
+
+  function additionTest(){
+    $.ajax({
+      url: '/addition',
+      type: 'POST',
+      data: values,
+      success: function(response){
+        answerOnDom(response);
+      }
+    }); //ends ajax
+  }
+
+  // values.type = 'subtraction';
+  // subtractionTest();
+  function subtractionTest(){
+    $.ajax({
+      url: '/subtraction',
+      type: 'POST',
+      data: values,
+      success: function(response){
+        answerOnDom(response);
+      }
+    }); //ends ajax
+  }
+
+  // values.type == 'multiplication';
+  // multiplicationTest();
+  function multiplicationTest(){
+    $.ajax({
+      url: '/multiplication',
+      type: 'POST',
+      data: values,
+      success: function(response){
+        answerOnDom(response);
+      }
+    }); //ends ajax
+  }
+
+  // values.type == 'division';
+  // divisionTest();
+  function divisionTest(){
+    $.ajax({
+      url: '/division',
+      type: 'POST',
+      data: values,
+      success: function(response){
+        answerOnDom(response);
+      }
+    }); //ends ajax
+  }
+  // console.log('math is working');
+
+});//ends doc ready
+
+function answerOnDom(answer){
+  $('#showAnswer').text(answer);
+}
+
+$('#clear').on('click', function(){
+  $('#showAnswer').text("");
+});
+// $('#numberContainer').on('click', function(){
+// var newOFishbject = {};
+// newFishObject.name = $('#newFishName').val();
+// $.ajax({
+//   type: 'POST',
+//   url: '/fish/new',
+//   data: newFishObject,
+//   success: function(response){
+// console.log(response);
+//     getFishData();
+//   }
+// }); //ends success response fxn
